d40a7b6318108a1f6f55f1569cbf6377c20ff115
Benjamin Llanes <Benjamin.Llanes@resmed.com>
2018-07-26 18:35:55 +0000

    Fixing data-loader for UCSD courses.

--- data-loaders/load_ucsd_courses.py
+++ data-loaders/load_ucsd_courses.py
@@ -8,17 +8,19 @@ UCSD_COURSE_CATALOG_MAIN_PAGE_URL = 'http://www.ucsd.edu/catalog/front/courses.h
 
 response = requests.get(UCSD_COURSE_CATALOG_MAIN_PAGE_URL)
 
-soup = BeautifulSoup(response.text, 'lxml')
+soup = BeautifulSoup(response.text, 'html.parser')
 
 a_tags = soup.findAll('a', string='courses')
 partial_urls = map(lambda a: a['href'][3:], a_tags)
 
 courses = []
+seen = set()
 
+pk = 1
 for partial_url in partial_urls:
     response = requests.get(UCSD_COURSE_CATALOG_BASE_URL + partial_url)
 
-    soup = BeautifulSoup(response.text, 'lxml')
+    soup = BeautifulSoup(response.text, 'html.parser')
 
     p_tags = soup.find_all('p', class_='course-name')
 
@@ -39,13 +41,26 @@ for partial_url in partial_urls:
         number = split2[1] 
         verbose_name = split1[1].strip()
 
-        course = {
+        if (department, number) in seen:
+            continue
+
+        seen.add((department, number))
+
+
+        course_fields = {
             'department': department,
             'number': number,
             'verbose_name': verbose_name
         }
 
+        course = {
+            'model': 'courses.course',
+            'pk': pk,
+            'fields': course_fields
+        }
+
         courses.append(course)
+        pk += 1
     
 with open(path.join(path.dirname(path.dirname(path.abspath(__file__))), 'data/ucsd_courses.json'), 'w+') as courses_file:
     json.dump(courses, courses_file)
