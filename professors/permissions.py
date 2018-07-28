6ff22a3928dd7f270d1b6ebd6a5b1b50b987b7d5
Ben Lalanes <chtka.ow@gmail.com>
2018-07-28 20:58:04 +0000

    Changed permissions to always require authentication for all methods.

--- professors/permissions.py
+++ professors/permissions.py
@@ -4,7 +4,7 @@ class IsAdminUserOrReadOnly(permissions.BasePermission):
     
     def has_permission(self, request, view):
         return (
-            request.user.username and            
-            (request.method in permissions.SAFE_METHODS or
+            request.user.username and
+            (request.method in permissions.SAFE_METHODS or 
             request.user.is_staff)
         )
