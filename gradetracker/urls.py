e2d9b3ff77a1acfaaa8b7a0d142de14b5fc71b5c
Ben Lalanes <chtka.ow@gmail.com>
2018-07-28 20:59:36 +0000

    Registered professors app in settings and urls.

--- gradetracker/urls.py
+++ gradetracker/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('courses/', include('courses.urls'))
+    path('courses/', include('courses.urls')),
+    path('professors/', include('professors.urls'))
 ]
