9f86f5aa62c9affce417de5ef687322712a6b50d
Ben Lalanes <chtka.ow@gmail.com>
2018-08-03 14:37:55 +0000

    Added classes model.

---
+++ classes/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+
+from courses.models import Course
+from professors.models import Professor
+
+ACADEMIC_QTR_CHOICES = (
+  ('FA', 'Fall'),
+  ('WI', 'Winter'),
+  ('SP', 'Spring'),
+  ('SS1', 'Summer Session 1'),
+  ('SS2', 'Summer Session 2'),
+)
+
+class Class(models.Model):
+  course = models.ForeignKey(Course, on_delete=models.CASCADE)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+  quarter = models.CharField(choices=ACADEMIC_QTR_CHOICES)
+  year = models.IntegerField()
+
+  grade = models.FloatField(default=100)
+
+
+# Create your models here.
