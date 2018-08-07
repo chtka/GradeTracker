4c65831f87b81a02233317644c1f7f6973a9ef6d
Ben Lalanes <chtka.ow@gmail.com>
2018-08-07 07:42:56 +0000

    Added professor views

--- professors/views.py
+++ professors/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django_filters import rest_framework as filters
 
 from rest_framework import generics, permissions
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
@@ -7,10 +7,18 @@ from professors.models import Professor
 from professors.serializers import ProfessorSerializer
 from professors.permissions import IsAdminUserOrReadOnly
 
+class ProfessorFilter(filters.FilterSet):
+
+    class Meta:
+        model = Professor
+        fields = ['first_name', 'last_name',]
+
 class ProfessorList(generics.ListCreateAPIView):
     queryset = Professor.objects.all()
     serializer_class = ProfessorSerializer
     permission_classes = (IsAdminUserOrReadOnly,)
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = ProfessorFilter
 
 class ProfessorDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Professor.objects.all()
